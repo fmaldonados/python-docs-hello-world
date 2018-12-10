c037b09632baa90ba5649f52fbcc6cbfabd6a4b5
fmaldonados <felixmaldonado@unitec.edu>
2018-12-09 22:59:02 -0600

    parentesis

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 URI = "mongodb://proyecto:1g1bO6iQ6iotdK3ncd5f3ud5YPmTCACu19ugCeoXLjuF3Zjciqygzlu7wu8exeNq9z7CuT4V4URNdPeEjAVrcw==@proyecto.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
 client = pymongo.MongoClient(URI)
 app.DB = client["chess"]
-print "Connected to Database"
+print ("Connected to Database")
 
 #Routes
 api = Api(app)
