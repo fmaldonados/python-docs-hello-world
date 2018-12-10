90c45d1d5eeecb5d3151488422d4779b71065090
fmaldonados <felixmaldonado@unitec.edu>
2018-12-09 22:52:48 -0600

    con mongo

--- application.py
+++ application.py
@@ -1,5 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+import pymongo
+
+URI = "mongodb://proyecto:1g1bO6iQ6iotdK3ncd5f3ud5YPmTCACu19ugCeoXLjuF3Zjciqygzlu7wu8exeNq9z7CuT4V4URNdPeEjAVrcw==@proyecto.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+client = pymongo.MongoClient(URI)
+app.DB = client["chess"]
+print ("Connected to Database")
 
 @app.route("/")
 def hello():
